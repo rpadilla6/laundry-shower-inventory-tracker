7a474ac8748603c2a04c88324a635492daf70594
Ryan Padilla <ryan.padilla@vincit.fi>
2024-02-04 00:37:54 -0800

    Enable tailwind forms plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./app/**/*.{ts,tsx,jsx,js}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/forms")],
 };
