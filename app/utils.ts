c4a983b5e5475be82e836385f6a3ae486e989126
Ryan Padilla <ryan.padilla@vincit.fi>
2024-02-05 20:54:06 -0800

    Fix time input bug
    toISOString() makes the date string UTC time from local, so my split
    method has a bit of a hard time. Thanks to the added function we good

--- app/utils.ts
+++ app/utils.ts
@@ -8,7 +8,7 @@ export function useMatchesData(id: string): any {
   const matchingRoutes = useMatches();
   const route = useMemo(
     () => matchingRoutes.find((route) => route.id === id),
-    [matchingRoutes, id]
+    [matchingRoutes, id],
   );
 
   return route?.data;
@@ -30,7 +30,7 @@ export function useUser() {
   const maybeUser = useOptionalUser();
   if (!maybeUser) {
     throw new Error(
-      "No user found in root loader, but user is required by useUser. If user is optional, try useOptionalUser instead."
+      "No user found in root loader, but user is required by useUser. If user is optional, try useOptionalUser instead.",
     );
   }
   return maybeUser;
@@ -41,33 +41,35 @@ export function validateEmail(email: unknown): email is string {
 }
 
 export function getEmojiStringForEntry(entry: DistributionRecord) {
-  const emoji:string[] = [];
-  if (entry.backpack)
-    emoji.push("🎒");
-  if (entry.duffel_bag)
-    emoji.push("👜");
-  if (entry.blanket)
-    emoji.push("🇶🇦");
-  if (entry.sleeping_bag)
-    emoji.push("🛌");
-  if (entry.tarp)
-    emoji.push("⛺️");
+  const emoji: string[] = [];
+  if (entry.backpack) emoji.push("🎒");
+  if (entry.duffel_bag) emoji.push("👜");
+  if (entry.blanket) emoji.push("🇶🇦");
+  if (entry.sleeping_bag) emoji.push("🛌");
+  if (entry.tarp) emoji.push("⛺️");
   return emoji.join(" ");
 }
 
 export const entrySchema = z.object({
-    first_name: z.string().min(2, "First name must be at least 2 characters"),
-    last_name: z.string().min(1, "Last name or initial is required"),
-    backpack: z.boolean().nullish(),
-    blanket: z.boolean().nullish(),
-    duffel_bag: z.boolean().nullish(),
-    tarp: z.boolean().nullish(),
-    sleeping_bag: z.boolean().nullish(),
-    profile_id: z.string({ required_error: "User profile is required" }),
-    distribution_date: z
-      .string()
-      .refine(
-        (value) => !isNaN(Date.parse(value)),
-        "Invalid distribution date",
-      ),
-  });
+  first_name: z.string().min(2, "First name must be at least 2 characters"),
+  last_name: z.string().min(1, "Last name or initial is required"),
+  backpack: z.boolean().nullish(),
+  blanket: z.boolean().nullish(),
+  duffel_bag: z.boolean().nullish(),
+  tarp: z.boolean().nullish(),
+  sleeping_bag: z.boolean().nullish(),
+  profile_id: z.string({ required_error: "User profile is required" }),
+  distribution_date: z
+    .string()
+    .refine((value) => !isNaN(Date.parse(value)), "Invalid distribution date"),
+});
+
+// Shoutout https://gist.github.com/loilo/736d5beaef4a96d652f585b1b678a12c
+export function getLocalISOString(date: Date) {
+  const offset = date.getTimezoneOffset();
+  const offsetAbs = Math.abs(offset);
+  const isoString = new Date(date.getTime() - offset * 60 * 1000).toISOString();
+  return `${isoString.slice(0, -1)}${offset > 0 ? "-" : "+"}${String(
+    Math.floor(offsetAbs / 60),
+  ).padStart(2, "0")}:${String(offsetAbs % 60).padStart(2, "0")}`;
+}
